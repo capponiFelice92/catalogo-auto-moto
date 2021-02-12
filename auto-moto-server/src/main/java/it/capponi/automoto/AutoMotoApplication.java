6863d7449cad440631946bd2ff20b21d91ccc8d8
capponiFelice92 <78529321+capponiFelice92@users.noreply.github.com>
2021-02-12 07:46:27 +0100

    inizializzato progetto

---
+++ auto-moto-server/src/main/java/it/capponi/automoto/AutoMotoApplication.java
@@ -0,0 +1,13 @@
+package it.capponi.automoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoMotoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoMotoApplication.class, args);
+	}
+
+}
---
+++ auto-moto-server/src/test/java/it/capponi/automoto/AutoMotoApplicationTests.java
@@ -0,0 +1,13 @@
+package it.capponi.automoto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoMotoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
